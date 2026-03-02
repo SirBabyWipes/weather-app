5d34b768ed79a3ad85fc928c8efc47f5b2b947fe
John Shanahan <jpshan2004@gmail.com>
2026-03-02 09:24:03 -0500

    swift porject

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by John Shanahan on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
