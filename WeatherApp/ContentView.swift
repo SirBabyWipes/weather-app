eaf2983872403564395cb3da985425663301a3a0
JSkyeV <swtorjsv@icloud.com>
2026-04-19 20:42:39 -0400

    delete unused fetchInitWeather in favor of new loadCachedWeather

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -312,20 +312,6 @@ struct ContentView: View {
     // Creates the API for all weatherdata related logic
     let api = WeatherAPI()
     
-    // Fetches the initial weather data on startup, uses userCache
-    func fetchInitWeather() async {
-        do {
-            let _ = try await api.getWeather(useCache: true)
-            print(UserDefaults.standard.dictionaryRepresentation()) // For debug purposes, to see the UserDefaults cache
-            // **** THIS WIPES THE APP'S CACHE - USE FOR DEBUG AND UNCOMMENT AS NEEDED
-            //if let bundleID = Bundle.main.bundleIdentifier {
-            //   UserDefaults.standard.removePersistentDomain(forName: bundleID)
-            //   UserDefaults.standard.synchronize()
-            //}
-        } catch {
-            print("Error", error)
-        }
-    }
     func fetchWeather() async {
         
         do {
@@ -338,6 +324,7 @@ struct ContentView: View {
             print("Error:", error)
         }
     }
+    // Fetches the initial weather data on startup, uses userCache
     func loadCachedWeather() async {
         
         do {
